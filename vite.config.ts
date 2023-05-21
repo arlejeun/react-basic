614e8f61496b23de377d0862ddccd5764229c6fc
Arnaud Lejeune <lejeune.arnaud@gmail.com>
2023-05-21 07:43:01 -0500

    clean build

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,46 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from "vite-plugin-dts";
 import * as path from 'path';
 import { existsSync } from 'fs';
 import * as dotenv from 'dotenv';
 
-// Dotenv 是一个零依赖的模块，它能将环境变量中的变量从 .env 文件加载到 process.env 中
 dotenv.config({
     path: existsSync('.env') ? '.env' : path.resolve('envs', `.env.${process.env.NODE_ENV}`)
 });
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [reactRefresh()],
-    resolve: {
-        alias: {
-            '@@': path.resolve(__dirname),
-            '@': path.resolve(__dirname, 'src')
+const viteBuild = ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    //process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    process.env = { ...loadEnv(mode, process.cwd()) };
+
+    console.log('Environemnts' + JSON.stringify(process.env))
+
+    // https://vitejs.dev/config/
+    const viteConfig = {
+        plugins: [
+            react(), 
+            dts({
+                insertTypesEntry: true,
+              }
+            )
+        ],
+        resolve: {
+            alias: {
+                '@@': path.resolve(__dirname),
+                '@': path.resolve(__dirname, 'src')
+            }
+        },
+        server: {
+            cors: true,
+            port: process.env.VITE_PORT as unknown as number,
+            hmr: {
+                host: 'localhost',
+                protocol: 'ws',
+                port: process.env.VITE_PORT as unknown as number
+            }
         }
-    },
-    server: {
-        cors: true,
-        port: process.env.VITE_PORT as unknown as number,
-        hmr: {
-            host: 'localhost',
-            protocol: 'ws',
-            port: process.env.VITE_PORT as unknown as number
-        }
-    }
-});
+    };
+    return defineConfig(viteConfig)
+};
+export default viteBuild;
